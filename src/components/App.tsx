b30df8dcc7e0676b5c2848c496529f9f4c5edb76
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2023-11-16 01:27:47 +0200

    start

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AppMenu from "./Header/AppMenu";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppMenu />
+      </header>
+    </div>
+  );
+}
+
+export default App;
